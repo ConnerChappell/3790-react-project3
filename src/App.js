b6ad7d495e0482d2bcc101ea4e92979dd590d3fb
Conner Chappell <conner.chappell123@gmail.com>
2021-11-09 15:45:04 -0700

    Added drawer and navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css'
-import EnglishPremierLeagePage from './pages/EnglishPremierLeagePage'
+import EnglishPremierLeagePage from './pages/EnglishPremierLeaguePage'
 import GermanBundesligaPage from './pages/GermanBundesligaPage'
 import { TeamContextProvider } from './contexts/TeamContext'
 import ButtonAppBar from './components/nav/ButtonAppBar'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import Home from './pages/Home'
 import LoginForm from './components/login/LoginForm'
 
@@ -13,10 +13,21 @@ const App = () => {
             <TeamContextProvider>
                 <ButtonAppBar />
 
-                <Route path="/home"><Home /></Route>
-                <Route path="/english-premier-league"><EnglishPremierLeagePage /></Route>
-                <Route path="/german-bundesliga"><GermanBundesligaPage /></Route>
-                <Route path="/login"><LoginForm /></Route>
+                <Route path="/" exact>
+                    <Redirect to="/home" />
+                </Route>
+                <Route path="/home">
+                    <Home />
+                </Route>
+                <Route path="/english-premier-league">
+                    <EnglishPremierLeagePage />
+                </Route>
+                <Route path="/german-bundesliga">
+                    <GermanBundesligaPage />
+                </Route>
+                <Route path="/login">
+                    <LoginForm />
+                </Route>
             </TeamContextProvider>
         </div>
     )
--- src/components/nav/ButtonAppBar.js
+++ src/components/nav/ButtonAppBar.js
@@ -6,22 +6,56 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
-// import Modal from '@mui/material/Modal'
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer'
+import {
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from '@mui/material'
+import Divider from '@mui/material/Divider'
 // import { useTeamContext } from '../../contexts/TeamContext'
-// import LoginForm from '../login/LoginForm'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom'
 
 const ButtonAppBar = () => {
     // const teamData = useTeamContext()
+    const history = useHistory()
+    const [isOpen, setIsOpen] = React.useState(false)
 
-    // const [open, setOpen] = React.useState(false)
-    // // Modal Functions
-    // const handleOpen = (team) => {
-    //     setOpen(true)
-    // }
-    // const handleClose = () => {
-    //     setOpen(false)
-    // }
+    const toggleDrawer = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const handleNavChoice = (pageLink) => {
+        history.push(`/${pageLink}`)
+        toggleDrawer()
+    }
+
+    const drawerItemList = () => (
+        <Box sx={{ width: 250 }} role="presentation">
+            <List>
+                <ListItem>
+                    <ListItemIcon>
+                        <SportsSoccerIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Menu" />
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem button onClick={() => handleNavChoice('home')}>
+                    <ListItemText primary="Home" />
+                </ListItem>
+                <ListItem button onClick={() => handleNavChoice('english-premier-league')}>
+                    <ListItemText primary="English Premier League" />
+                </ListItem>
+                <ListItem button onClick={() => handleNavChoice('german-bundesliga')}>
+                    <ListItemText primary="German Bundesliga" />
+                </ListItem>
+            </List>
+        </Box>
+    )
 
     return (
         <>
@@ -38,7 +72,8 @@ const ButtonAppBar = () => {
                             edge="start"
                             color="inherit"
                             aria-label="menu"
-                            sx={{ mr: 2 }}>
+                            sx={{ mr: 2 }}
+                            onClick={toggleDrawer}>
                             <MenuIcon />
                         </IconButton>
                         <Typography
@@ -54,9 +89,9 @@ const ButtonAppBar = () => {
                 </AppBar>
             </Box>
 
-            {/* <Modal open={open}>
-                <LoginForm closeHandler={handleClose}/>
-            </Modal> */}
+            <Drawer anchor="left" open={isOpen} onClose={toggleDrawer}>
+                {drawerItemList()}
+            </Drawer>
         </>
     )
 }
