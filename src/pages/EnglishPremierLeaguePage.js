47c4a31ed367c5d3d4f83cb8cf8ae753a75bcfb5
Conner Chappell <conner.chappell123@gmail.com>
2021-11-12 19:05:24 -0700

    Added logos and styled team page headers

--- src/pages/EnglishPremierLeaguePage.js
+++ src/pages/EnglishPremierLeaguePage.js
@@ -1,10 +1,36 @@
 import * as React from 'react'
+import EnglishLogo from '../assets/premier-league-logo.png'
 import EnglishTeamContainer from '../components/EnglishTeamContainer'
+import { Box, Typography, CardMedia } from '@mui/material'
 
 const EnglishPremierLeagePage = () => {
     return (
         <div>
-            <h1>English Premier Leage</h1>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100&',
+                    height: 'auto',
+                    p: 3,
+                    bgcolor: 'background.paper',
+                    boxShadow: 3,
+                }}>
+                <CardMedia
+                    component="img"
+                    image={EnglishLogo}
+                    alt="English Premier League Logo"
+                    sx={{
+                        width: 'auto',
+                        height: 60,
+                        mr: '10px',
+                    }}
+                />
+                <Typography component="h1" variant="h3">
+                    English Premier League
+                </Typography>
+            </Box>
 
             <EnglishTeamContainer />
         </div>
--- src/pages/GermanBundesligaPage.js
+++ src/pages/GermanBundesligaPage.js
@@ -1,10 +1,36 @@
 import * as React from 'react'
+import GermanLogo from '../assets/german-bundesliga-logo.png'
 import GermanTeamContainer from '../components/GermanTeamContainer'
+import { Box, CardMedia, Typography} from '@mui/material'
 
 const GermanBundesligaPage = () => {
     return (
         <div>
-            <h1>German Bundesliga</h1>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100&',
+                    height: 'auto',
+                    p: 3,
+                    bgcolor: 'background.paper',
+                    boxShadow: 3,
+                }}>
+                <CardMedia
+                    component="img"
+                    image={GermanLogo}
+                    alt="English Premier League Logo"
+                    sx={{
+                        width: 'auto',
+                        height: 60,
+                        mr: '10px',
+                    }}
+                />
+                <Typography component="h1" variant="h3">
+                    German Bundesliga
+                </Typography>
+            </Box>
 
             <GermanTeamContainer />
         </div>
