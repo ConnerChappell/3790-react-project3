c230fdc713d910151b700b9c7f2a7d32e71f3c7e
Conner Chappell <conner.chappell123@gmail.com>
2021-11-13 15:53:32 -0700

    Initial header styled for team detail page

--- src/pages/TeamDetail.js
+++ src/pages/TeamDetail.js
@@ -1,16 +1,41 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom'
 import { useTeamContext } from '../contexts/TeamContext'
+import { Box, CardMedia, Typography } from '@mui/material'
 
 const TeamDetail = () => {
     const params = useParams()
     const teamData = useTeamContext()
 
-    const team = teamData.allTeams.find(item => item.idTeam === params.teamId)
+    const team = teamData.allTeams.find((item) => item.idTeam === params.teamId)
     console.log(team)
 
     return (
-        <h1>{team.strTeam}</h1>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100&',
+                height: 'auto',
+                p: 2,
+                bgcolor: 'background.paper',
+                boxShadow: 3,
+            }}>
+            <CardMedia
+                component="img"
+                image={team.strTeamBadge}
+                alt="Team Badge"
+                sx={{
+                    width: 70,
+                    height: 'auto',
+                    mr: 1,
+                }}
+            />
+            <Typography component="h1" variant="h3">
+                {team.strTeam}
+            </Typography>
+        </Box>
     )
 }
 
