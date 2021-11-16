32b04cf42157937adef7e7133f2b1e79d7d838ba
Conner Chappell <conner.chappell123@gmail.com>
2021-11-15 19:20:43 -0700

    Added suspense from react and spinning loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, { Suspense } from 'react'
 import './App.css'
 import Home from './pages/Home'
-import EnglishPremierLeagePage from './pages/EnglishPremierLeaguePage'
-import GermanBundesligaPage from './pages/GermanBundesligaPage'
 import TeamDetail from './pages/TeamDetail'
 import NotFound from './pages/NotFound'
 import ButtonAppBar from './components/nav/ButtonAppBar'
 import LoginForm from './components/login/LoginForm'
 import { TeamContextProvider } from './contexts/TeamContext'
 import { Route, Redirect, Switch } from 'react-router-dom'
+import { Box, CircularProgress } from '@mui/material'
+
+const EnglishPremierLeagePage = React.lazy(() =>
+    import('./pages/EnglishPremierLeaguePage')
+)
+const GermanBundesligaPage = React.lazy(() =>
+    import('./pages/GermanBundesligaPage')
+)
 
 const App = () => {
     return (
@@ -15,30 +22,41 @@ const App = () => {
             <TeamContextProvider>
                 <ButtonAppBar />
 
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/home" />
-                    </Route>
-                    <Route path="/home">
-                        <Home />
-                    </Route>
-                    <Route path="/english-premier-league">
-                        <EnglishPremierLeagePage />
-                    </Route>
-                    <Route path="/german-bundesliga">
-                        <GermanBundesligaPage />
-                    </Route>
-                    <Route path="/team/:teamId" exact>
-                        <TeamDetail />
-                    </Route>
-                    <Route path="/login">
-                        <LoginForm />
-                    </Route>
-                    <Route path="*">
-                        <NotFound />
-                    </Route>
-                </Switch>
-                
+                <Suspense
+                    fallback={
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                mt: 5,
+                            }}>
+                            <CircularProgress color="error" />
+                        </Box>
+                    }>
+                    <Switch>
+                        <Route path="/" exact>
+                            <Redirect to="/home" />
+                        </Route>
+                        <Route path="/home">
+                            <Home />
+                        </Route>
+                        <Route path="/english-premier-league">
+                            <EnglishPremierLeagePage />
+                        </Route>
+                        <Route path="/german-bundesliga">
+                            <GermanBundesligaPage />
+                        </Route>
+                        <Route path="/team/:teamId" exact>
+                            <TeamDetail />
+                        </Route>
+                        <Route path="/login">
+                            <LoginForm />
+                        </Route>
+                        <Route path="*">
+                            <NotFound />
+                        </Route>
+                    </Switch>
+                </Suspense>
             </TeamContextProvider>
         </div>
     )
