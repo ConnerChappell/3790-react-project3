d9dd847e5e2fb8a392b762aa9753052737292fb5
Conner Chappell <conner.chappell123@gmail.com>
2021-11-21 21:13:32 -0700

    Styled confirmation page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,18 @@ import { Box, Typography, Card, CardMedia, CardContent } from '@mui/material'
 import HelpIcon from '@mui/icons-material/Help'
 import HeaderImage from '../assets/soccer-app-header.jpg'
 
+const ContainerStyle = {
+    display: 'flex',
+    alignItems: 'start',
+    flexDirection: 'column',
+    maxWidth: '1400px',
+    width: 'auto',
+    margin: '0 auto',
+    p: 0,
+    bgcolor: 'background.paper',
+    boxShadow: 3,
+}
+
 const InitialCardStyle = {
     display: 'flex',
     flexDirection: 'column',
@@ -29,18 +41,7 @@ const Home = () => {
         <>
             {!identity.provisionalUser && !identity.user && (
                 // Page Container
-                <Box
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'start',
-                        flexDirection: 'column',
-                        maxWidth: '1400px',
-                        width: 'auto',
-                        margin: '0 auto',
-                        p: 0,
-                        bgcolor: 'background.paper',
-                        boxShadow: 3,
-                    }}>
+                <Box sx={ContainerStyle}>
                     <Box
                         sx={{
                             position: 'relative',
@@ -176,13 +177,141 @@ const Home = () => {
             )}
 
             {identity.provisionalUser && (
-                <Box>
-                    <h1>Thanks for signing up!</h1>
-                    <h2>
-                        Please check your email at "
-                        {identity.provisionalUser.email}" to confirm your
-                        account
-                    </h2>
+                // Page Container
+                <Box sx={ContainerStyle}>
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            color: '#fff',
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'center',
+                            backgroundImage: `url(${HeaderImage})`,
+                            width: '100%',
+                            height: '400px',
+                        }}>
+                        <Box
+                            sx={{
+                                position: 'absolute',
+                                top: 0,
+                                bottom: 0,
+                                right: 0,
+                                left: 0,
+                                backgroundColor: 'rgba(0,0,0,.3)',
+                            }}
+                        />
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            gutterBottom
+                            sx={{
+                                fontWeight: 'bold',
+                                textShadow: '2px 3px 5px rgba(0,0,0,0.5)',
+                                zIndex: '99',
+                                mx: 'auto',
+                            }}>
+                            Thanks for signing up!
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            paragraph
+                            sx={{
+                                fontWeight: 'bold',
+                                textShadow: '2px 3px 5px rgba(0,0,0,0.5)',
+                                zIndex: '99',
+                                mb: 6,
+                            }}>
+                            Please check your email at "
+                            {identity.provisionalUser.email}" to confirm your
+                            account
+                        </Typography>
+                    </Box>
+
+                    <Typography
+                        component="h2"
+                        variant="h3"
+                        sx={{ mx: 'auto', mt: 4 }}>
+                        Leagues
+                    </Typography>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                            textAlign: 'center',
+                            mb: 5,
+                        }}>
+                        <Card sx={InitialCardStyle}>
+                            <HelpIcon
+                                sx={{
+                                    fontSize: 200,
+                                }}
+                            />
+                            <Typography variant="h5">
+                                Signup or login to visit
+                            </Typography>
+                        </Card>
+
+                        <Card sx={InitialCardStyle}>
+                            <HelpIcon
+                                sx={{
+                                    fontSize: 200,
+                                }}
+                            />
+                            <Typography variant="h5">
+                                Signup or login to visit
+                            </Typography>
+                        </Card>
+
+                        <Card sx={InitialCardStyle}>
+                            <HelpIcon
+                                sx={{
+                                    fontSize: 200,
+                                }}
+                            />
+                            <Typography variant="h5">
+                                Signup or login to visit
+                            </Typography>
+                        </Card>
+
+                        <Card sx={InitialCardStyle}>
+                            <HelpIcon
+                                sx={{
+                                    fontSize: 200,
+                                }}
+                            />
+                            <Typography variant="h5">
+                                Signup or login to visit
+                            </Typography>
+                        </Card>
+
+                        <Card sx={InitialCardStyle}>
+                            <HelpIcon
+                                sx={{
+                                    fontSize: 200,
+                                }}
+                            />
+                            <Typography variant="h5">
+                                Signup or login to visit
+                            </Typography>
+                        </Card>
+
+                        <Card sx={InitialCardStyle}>
+                            <HelpIcon
+                                sx={{
+                                    fontSize: 200,
+                                }}
+                            />
+                            <Typography variant="h5">
+                                Signup or login to visit
+                            </Typography>
+                        </Card>
+                    </Box>
                 </Box>
             )}
 
