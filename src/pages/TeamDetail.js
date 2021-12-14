56118313ed857e75d55cc36c82b92a47f8db10bc
Conner Chappell <conner.chappell123@gmail.com>
2021-12-13 18:06:15 -0700

    Fade transition added to team detail page

--- src/pages/TeamDetail.js
+++ src/pages/TeamDetail.js
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 import { useSoccerContext } from '../contexts/SoccerContext'
-import { Box, CardMedia, Typography, IconButton, Tooltip } from '@mui/material'
+import {
+    Box,
+    CardMedia,
+    Typography,
+    IconButton,
+    Tooltip,
+    Fade,
+} from '@mui/material'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
 const TeamDetail = () => {
@@ -9,154 +16,160 @@ const TeamDetail = () => {
     const soccerData = useSoccerContext()
     const history = useHistory()
 
-    const team = soccerData.allTeams.find((item) => item.idTeam === params.teamId)
+    const team = soccerData.allTeams.find(
+        (item) => item.idTeam === params.teamId
+    )
     console.log(team)
 
     return (
         <>
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100&',
-                    height: 'auto',
-                    p: 2,
-                    bgcolor: 'background.paper',
-                    boxShadow: 3,
-                }}>
-                <Tooltip title="Go Back">
-                    <IconButton sx={{mr: 1}} onClick={history.goBack}>
-                        <ArrowBackIcon />
-                    </IconButton>
-                </Tooltip>
-                <CardMedia
-                    component="img"
-                    image={team.strTeamBadge}
-                    alt="Team Badge"
-                    sx={{
-                        width: 70,
-                        height: 'auto',
-                        mr: 1,
-                    }}
-                />
-                <Typography component="h1" variant="h3">
-                    {team.strTeam}
-                </Typography>
-            </Box>
-
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    flexWrap: 'wrap',
-                    justifyContent: 'space-between',
-                    maxWidth: '1400px',
-                    mx: 'auto',
-                    pb: 10,
-                }}>
-                {/* Sidebar stuff */}
+            <Fade in timeout={1500}>
                 <Box
                     sx={{
                         display: 'flex',
-                        flexDirection: 'column',
-                        width: '375px',
-                        mt: 4,
-                        mx: 'auto',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '100&',
+                        height: 'auto',
+                        p: 2,
                         bgcolor: 'background.paper',
-                        borderRadius: 1,
-                        boxShadow: 4,
+                        boxShadow: 3,
                     }}>
-                    <Box sx={{ m: 2, mb: 5 }}>
-                        <CardMedia
-                            component="img"
-                            image={team.strTeamLogo}
-                            alt="Team Logo"
-                            sx={{
-                                width: 200,
-                                height: 'auto',
-                                mx: 'auto',
-                            }}></CardMedia>
-                    </Box>
-
-                    <Box sx={{ m: 2, mb: 5 }}>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Location: {team.strStadiumLocation}
-                        </Typography>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Established in {team.intFormedYear}
-                        </Typography>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Nicknames: {team.strKeywords}
-                        </Typography>
-                    </Box>
-
-                    <Box sx={{ m: 2, mb: 5 }}>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Jersey
-                        </Typography>
-                        <CardMedia
-                            component="img"
-                            image={team.strTeamJersey}
-                            alt="Team Jersey"
-                            sx={{
-                                width: 200,
-                                height: 'auto',
-                                mx: 'auto',
-                            }}></CardMedia>
-                    </Box>
-
-                    <Box sx={{ m: 2, mb: 5 }}>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Stadium: {team.strStadium}
-                        </Typography>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Capacity: {team.intStadiumCapacity}
-                        </Typography>
-                        <CardMedia
-                            component="img"
-                            image={team.strStadiumThumb}
-                            alt="Team Stadium"
-                            sx={{
-                                width: 300,
-                                height: 'auto',
-                                mx: 'auto',
-                            }}></CardMedia>
-                    </Box>
+                    <Tooltip title="Go Back">
+                        <IconButton sx={{ mr: 1 }} onClick={history.goBack}>
+                            <ArrowBackIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <CardMedia
+                        component="img"
+                        image={team.strTeamBadge}
+                        alt="Team Badge"
+                        sx={{
+                            width: 70,
+                            height: 'auto',
+                            mr: 1,
+                        }}
+                    />
+                    <Typography component="h1" variant="h3">
+                        {team.strTeam}
+                    </Typography>
                 </Box>
+            </Fade>
 
-                {/* Main stuff */}
+            <Fade in timeout={1500}>
                 <Box
                     sx={{
                         display: 'flex',
-                        flexDirection: 'column',
-                        width: '975px',
-                        height: 'auto',
-                        mt: 4,
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-between',
+                        maxWidth: '1400px',
                         mx: 'auto',
-                        bgcolor: 'background.paper',
-                        borderRadius: 1,
-                        boxShadow: 4,
+                        pb: 10,
                     }}>
-                    <Box sx={{ m: 2, mb: 5 }}>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Description:
-                        </Typography>
-                        <Typography variant="body1" sx={{ mb: 1 }}>
-                            {team.strDescriptionEN}
-                        </Typography>
+                    {/* Sidebar stuff */}
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            width: '375px',
+                            mt: 4,
+                            mx: 'auto',
+                            bgcolor: 'background.paper',
+                            borderRadius: 1,
+                            boxShadow: 4,
+                        }}>
+                        <Box sx={{ m: 2, mb: 5 }}>
+                            <CardMedia
+                                component="img"
+                                image={team.strTeamLogo}
+                                alt="Team Logo"
+                                sx={{
+                                    width: 200,
+                                    height: 'auto',
+                                    mx: 'auto',
+                                }}></CardMedia>
+                        </Box>
+
+                        <Box sx={{ m: 2, mb: 5 }}>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Location: {team.strStadiumLocation}
+                            </Typography>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Established in {team.intFormedYear}
+                            </Typography>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Nicknames: {team.strKeywords}
+                            </Typography>
+                        </Box>
+
+                        <Box sx={{ m: 2, mb: 5 }}>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Jersey
+                            </Typography>
+                            <CardMedia
+                                component="img"
+                                image={team.strTeamJersey}
+                                alt="Team Jersey"
+                                sx={{
+                                    width: 200,
+                                    height: 'auto',
+                                    mx: 'auto',
+                                }}></CardMedia>
+                        </Box>
+
+                        <Box sx={{ m: 2, mb: 5 }}>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Stadium: {team.strStadium}
+                            </Typography>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Capacity: {team.intStadiumCapacity}
+                            </Typography>
+                            <CardMedia
+                                component="img"
+                                image={team.strStadiumThumb}
+                                alt="Team Stadium"
+                                sx={{
+                                    width: 300,
+                                    height: 'auto',
+                                    mx: 'auto',
+                                }}></CardMedia>
+                        </Box>
                     </Box>
 
-                    <Box sx={{ m: 2, mb: 5 }}>
-                        <Typography variant="h6" sx={{ mb: 1 }}>
-                            Stadium Description:
-                        </Typography>
-                        <Typography variant="body1" sx={{ mb: 1 }}>
-                            {team.strStadiumDescription}
-                        </Typography>
+                    {/* Main stuff */}
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            width: '975px',
+                            height: 'auto',
+                            mt: 4,
+                            mx: 'auto',
+                            bgcolor: 'background.paper',
+                            borderRadius: 1,
+                            boxShadow: 4,
+                        }}>
+                        <Box sx={{ m: 2, mb: 5 }}>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Description:
+                            </Typography>
+                            <Typography variant="body1" sx={{ mb: 1 }}>
+                                {team.strDescriptionEN}
+                            </Typography>
+                        </Box>
+
+                        <Box sx={{ m: 2, mb: 5 }}>
+                            <Typography variant="h6" sx={{ mb: 1 }}>
+                                Stadium Description:
+                            </Typography>
+                            <Typography variant="body1" sx={{ mb: 1 }}>
+                                {team.strStadiumDescription}
+                            </Typography>
+                        </Box>
                     </Box>
                 </Box>
-            </Box>
+            </Fade>
         </>
     )
 }
